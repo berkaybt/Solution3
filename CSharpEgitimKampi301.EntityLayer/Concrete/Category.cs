629500d4a316ae975557756adf7c165c5f954efa
BerkayTepe <21197540+berkaybt@users.noreply.github.com>
2024-12-17 21:47:37 +0300

    12 update

--- CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -12,6 +12,10 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
         public string CategoryName { get; set; }
         public bool CategoryStatus  { get; set; }
 
+        public List<Product> Products { get; set; }
+
+
+
 
     }
 }
